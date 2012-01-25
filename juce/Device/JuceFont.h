8f2eefc14db9f80f7ba91a513eeea4dfee855048
Dominique Fober <fober@grame.fr>
2012-01-25 18:12:12 +0100

    juce device (in progress)

---
+++ juce/Device/JuceFont.h
@@ -0,0 +1,48 @@
+#ifndef __JuceFont__
+#define __JuceFont__
+
+/*
+	GUIDO Library
+	Copyright (C) 2012	Grame
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License (Version 2), 
+	as published by the Free Software Foundation.
+	A copy of the license can be found online at www.gnu.org/licenses.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+*/
+
+#include <string>
+#include "VGFont.h"
+
+class juce::Font;
+// --------------------------------------------------------------
+// 		Juce implementation of the VGFont class
+// --------------------------------------------------------------
+class JuceFont : public VGFont
+{
+	std::string	fName;
+	juce::Font * fNativeFont;
+
+	public:
+								 JuceFont(const char * faceName, int size, int properties);
+		virtual					~JuceFont();
+
+		// - VGFont services ---------------------------------------------
+		virtual	const char *	GetName() const;
+		virtual	int 			GetSize() const;
+		virtual	int				GetProperties() const;
+
+		// - Symbol services ---------------------------------------------
+		virtual	void			GetExtent( const char * s, int count, float * width, float * height, VGDevice *) const;
+		virtual	void			GetExtent( unsigned char c, float * width, float * height,  VGDevice *) const;
+
+		const Font&	NativeFont() const	{ return *fNativeFont; };
+
+};
+
+#endif
---
+++ juce/Device/JuceSystem.cpp
@@ -0,0 +1,67 @@
+/*
+	GUIDO Library
+	Copyright (C) 2012	Grame
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License (Version 2), 
+	as published by the Free Software Foundation.
+	A copy of the license can be found online at www.gnu.org/licenses.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+*/
+
+
+#include "JuceHeader.h"
+#include "JuceFont.h"
+#include "JuceSystem.h"
+#include "JuceDevice.h"
+
+using namespace juce;
+
+// --------------------------------------------------------------
+// 		Juce implementation of the VGSystem class
+// --------------------------------------------------------------
+VGDevice* JuceSystem::CreateDisplayDevice( )
+{
+	return new JuceDevice (fGraphics, this);
+}
+
+VGDevice* JuceSystem::CreateMemoryDevice( int width, int height )
+{
+	return new JuceDevice (width, height, this);
+}
+
+VGDevice* JuceSystem::CreateMemoryDevice( const char * path )
+{
+//	FileInputStream file (File (path));
+//	if (file.getStatus().failed()) return 0;
+//
+//	Image img;
+//	GIFImageFormat gif;
+//	JPEGImageFormat jpg; 
+//	PNGImageFormat png;
+//	if (gif.canUnderstand (file))
+//		img = gif.decodeImage (file);
+//	else if (jpg.canUnderstand (file))
+//		img = jpg.decodeImage (file);
+//	else if (png.canUnderstand (file))
+//		Image img = png.decodeImage (file);
+//	else return 0;
+//
+//	Graphics * g = new Graphics(img);
+//	return new JuceDevice (g, this);
+	return 0;
+}
+
+VGDevice* JuceSystem::CreatePrinterDevice( )	{ return 0;	}	// don't know how to create a printer device
+VGDevice* JuceSystem::CreateAntiAliasedMemoryDevice( int width, int height )	{ return CreateMemoryDevice(width, height); }
+
+// - Font services -----------------------------------------------
+const VGFont*	JuceSystem::CreateVGFont( const char * faceName, int size, int properties ) const
+{
+	return new JuceFont(faceName, size, properties);
+}
+
