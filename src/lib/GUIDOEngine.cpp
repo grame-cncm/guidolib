705f4f79051a36af8409c639d7ad25aced9e75fb
Dominique Fober <fober@grame.fr>
2012-02-14 10:20:24 +0100

    version number moved to 1.4.8

--- src/lib/GUIDOEngine.cpp
+++ src/lib/GUIDOEngine.cpp
@@ -74,8 +74,8 @@ using namespace std;
 // ==========================================================================
 const int GUIDOENGINE_MAJOR_VERSION = 1;
 const int GUIDOENGINE_MINOR_VERSION = 4;
-const int GUIDOENGINE_SUB_VERSION =	71;
-const char* GUIDOENGINE_VERSION_STR = "1.4.71";
+const int GUIDOENGINE_SUB_VERSION =	8;
+const char* GUIDOENGINE_VERSION_STR = "1.4.8";
 
 // global factory object, used by the parser and the GuidoFactory API
 ARFactory * gGlobalFactory = 0;
