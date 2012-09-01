bca54cc2ada975c63e6631dfe3172f3d256819a8
Mike Solomon <mike@apollinemike.com>
2012-09-01 09:04:50 +0200

    d

---
+++ juce/GuidoServer/Source/Main.cpp
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a Juce application.
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+#include <iostream>
+using namespace std;
+//==============================================================================
+int main (int argc, char* argv[])
+{
+
+    // ..your code goes here!
+
+    cout << "Hello world" << endl;
+    return 0;
+}
