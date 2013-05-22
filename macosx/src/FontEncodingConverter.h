affefa5e78033e7050c63f1b516af7f6131eb34b
Dominique Fober <fober@grame.fr>
2013-05-22 14:46:53 +0200

    license header changes

--- macosx/src/FontEncodingConverter.h
+++ macosx/src/FontEncodingConverter.h
@@ -1,10 +1,13 @@
 /*
-	FontEncodingConverter.h
+  GUIDO Library
+  Copyright (C) 2013  Grame
 
-	Convert strings between different encodings.
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
 
 */
 
