ecaf696558b4dbdadc6fbc77248f92482498b530
decron <decron@grame.fr>
2013-11-13 16:00:22 +0100

    From now, double bar adapts itself to staffFormat (as the finishBar). Regression-tests added.

--- src/graphic/GRDoubleBar.cpp
+++ src/graphic/GRDoubleBar.cpp
@@ -29,6 +29,8 @@ GRDoubleBar::GRDoubleBar( ARDoubleBar * ardbar, GRStaff * inStaff, const TYPE_TI
 	mBoundingBox.left  -= spacing;
 	mBoundingBox.right += spacing;
 	mRightSpace = mBoundingBox.right * mTagSize;
+
+    fLineNumber = inStaff->getNumlines();
 }
 
 GRDoubleBar::GRDoubleBar(ARDoubleBar * ardbar, GRSystem * p_grsystem, GRStaff * inStaff,
@@ -40,6 +42,8 @@ GRDoubleBar::GRDoubleBar(ARDoubleBar * ardbar, GRSystem * p_grsystem, GRStaff * 
 	mBoundingBox.left  -= spacing;
 	mBoundingBox.right += spacing;
 	mRightSpace = mBoundingBox.right * mTagSize;
+
+    fLineNumber = inStaff->getNumlines();
 }
 
 GRDoubleBar::~GRDoubleBar()
@@ -52,10 +56,54 @@ void GRDoubleBar::DrawWithLines( VGDevice & hdc ) const
 	if ((getTagType() != GRTag::SYSTEMTAG) && isSystemSlice())
 		return;			// don't draw staff bars on system slices
 
+    /* Vertical adjustement according to staff's line number */
+    float offsety1 = 0;
+    float offsety2 = 0;
+
+    if (fLineNumber == 0)
+    {
+        offsety1 = - LSPACE / 2;
+    }
+    else if (fLineNumber == 1)
+    {
+        offsety1 = - LSPACE;
+    }
+    else if (fLineNumber == 2)
+    {
+        offsety1 = LSPACE * (float)1.5;
+        offsety2 = - 3 * LSPACE;
+
+    }
+    else if (fLineNumber == 3)
+    {
+        offsety1 = LSPACE;
+        offsety2 = - 2 * LSPACE;
+
+    }
+    else if (fLineNumber == 4)
+    {
+        offsety1 = LSPACE / 2;
+        offsety2 = - LSPACE;
+
+    }
+    else if (fLineNumber == 6)
+    {
+        offsety1 = - LSPACE / 2;
+        offsety2 = LSPACE;
+
+    }
+    else if (fLineNumber == 7)
+    {
+        offsety1 = - LSPACE;
+        offsety2 = 2 * LSPACE;
+    }
+    /*********************************************************/
+
 	const float x1 = mPosition.x + mBoundingBox.left;
 	const float x2 = mPosition.x + mBoundingBox.right;
-	const float y1 = mPosition.y;
-	const float y2 = y1 + mBoundingBox.bottom;
+	const float y1 = mPosition.y + offsety1 + 1;
+	const float y2 = y1 + mBoundingBox.bottom + offsety2 - 2;
+
 	hdc.PushPenWidth( kLineThick * 1.2f);
 	hdc.Line( x1, y1, x1, y2 );
 	hdc.Line( x2, y1, x2, y2 );
