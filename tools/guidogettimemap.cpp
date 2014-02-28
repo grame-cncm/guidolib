f00dc4f9b2d5b8f40233f05326acf435a1b25649
D. Fober <fober@grame.fr>
2014-02-28 11:42:06 +0100

    tests extended to include map files

--- tools/guidogettimemap.cpp
+++ tools/guidogettimemap.cpp
@@ -32,18 +32,18 @@ static void error (GuidoErrCode err)
 	cerr << "error #" << err << ": " << GuidoGetErrorString (err) << endl;
 }
 
-static void draw(GRHandler gr, int page, VGDevice* dev)
+
+//------------------------------------------------------------------------------
+// time mapping collector abstract class definition
+class TimeMapPrinter : public TimeMapCollector
 {
-	GuidoOnDrawDesc desc;
-	desc.handle = gr;
-	desc.hdc = dev;
-	desc.page = page;
-	desc.updateRegion.erase = true;
-	desc.scrollx = desc.scrolly = 0;
-	desc.sizex = desc.sizey = kSize;
-	GuidoResizePageToMusic( gr );
-	GuidoOnDraw (&desc);
-}
+	public:
+		virtual ~TimeMapPrinter() {}
+		virtual void Time2TimeMap( const TimeSegment& from, const TimeSegment& to ) {
+			cout << from << " " << to << endl;
+		}
+};
+
 
 int main(int argc, char **argv)
 {
@@ -51,48 +51,27 @@ int main(int argc, char **argv)
     GuidoInitDesc gd = { dev, 0, 0, 0 };
     GuidoInit (&gd);
 
-	for (int i = 1; i < argc; i++)
-    {
-		cerr << "# get page mapping for " << argv[i] << endl;
+	for (int i=1; i < argc; i++) {
+		cerr << "# get time map for " << argv[i] << endl;
 		GuidoErrCode err;
 		ARHandler arh;
-        
         GuidoParser *parser = GuidoOpenParser();
 
         std::ifstream ifs(argv[i], ios::in);
-        if (!ifs) return 0;
+        if (!ifs)
+            return 0;
 
         std::stringstream streamBuffer;
         streamBuffer << ifs.rdbuf();
         ifs.close();
 
         arh = GuidoString2AR(parser, streamBuffer.str().c_str());
+
 		if (arh)
         {
-			GRHandler grh;
-			err = GuidoAR2GR (arh, 0, &grh);
-
-			if (err != guidoNoErr)
-                error(err);
-			else {
-				int n = GuidoGetPageCount (grh);
-
-				for (int page = 1; page <= n; page++)
-                {
-					draw (grh, page, dev);
-					Time2GraphicMap map;
-					cout << "# page " << page << "/" << n << endl;
-					cout << "######################### " << endl;
-					err = GuidoGetPageMap(grh, page, kSize, kSize, map);
-					if (err == guidoNoErr) {
-						for (Time2GraphicMap::const_iterator m = map.begin(); m != map.end(); m++) {
-							cout << m->first << " " << m->second << endl;
-						}
-					}
-					else error (err);
-				}
-				GuidoFreeGR (grh);
-			}
+			TimeMapPrinter mp;
+			err = GuidoGetTimeMap(arh, mp);
+			if (err != guidoNoErr) error (err);
 			GuidoFreeAR (arh);
 		}
 		else {
