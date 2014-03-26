796785451e3b3399e0f2ae2c79707bd27d3b15b1
Mike Solomon <mike@mikesolomon.org>
2014-03-26 10:04:06 +0200

    Adds android makefile for guido engine.

---
+++ android-apps/guido-engine-android/jni/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := GUIDOEngine
+
+LOCAL_SRC_FILES 	:= $(addprefix ../, $(wildcard ../../src/engine/[^d]*/*.cpp))
+LOCAL_C_INCLUDES 	:= $(wildcard ../../src/engine/*)
+LOCAL_CPPFLAGS		:= -Dandroid -frtti
+
+#$(warning Value of LOCAL_SRC_FILES is '$(LOCAL_SRC_FILES)')
+#$(warning Value of LOCAL_PATH is '$(LOCAL_PATH)')
+include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_STATIC_LIBRARY)
---
+++ android-apps/guido-engine-android/jni/Application.mk
@@ -0,0 +1,2 @@
+
+APP_STL 			:= stlport_static
