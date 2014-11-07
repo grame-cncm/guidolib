3fd980c404d326c3fd05aa99a3180f9fda9202f2
D. Fober <fober@grame.fr>
2014-11-07 12:15:55 +0100

    version number moved to 1.56

--- src/engine/lib/GUIDOEngine.cpp
+++ src/engine/lib/GUIDOEngine.cpp
@@ -76,8 +76,8 @@ using namespace std;
 // ==========================================================================
 const int GUIDOENGINE_MAJOR_VERSION = 1;
 const int GUIDOENGINE_MINOR_VERSION = 5;
-const int GUIDOENGINE_SUB_VERSION   = 5;
-const char* GUIDOENGINE_VERSION_STR = "1.5.5";
+const int GUIDOENGINE_SUB_VERSION   = 6;
+const char* GUIDOENGINE_VERSION_STR = "1.5.6";
 
 ARPageFormat * gARPageFormat = 0;
 
