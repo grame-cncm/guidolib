d61b9944ada6d11970d6511829d6a476041b0467
guillaume <guillaume.gouilloux@gmail.com>
2014-11-20 13:08:23 +0100

    refactoring

--- javascript/js/guidoEmscriptenBridge.js
+++ javascript/js/guidoEmscriptenBridge.js
@@ -24,7 +24,7 @@ GuidoGetDuration = Module.cwrap('GuidoGetDuration','number', ['number']);
 
 GuidoFindEventPage_p = Module.cwrap('GuidoFindEventPage_p','number', ['number','number']);
 GuidoFindPageAt_p = Module.cwrap('GuidoFindPageAt_p','number', ['number','number']);
-GuidoGetDatePageDate = Module.cwrap('GuidoGetDatePageDate','number', ['number','number']);
+GuidoGetNewPageDate = Module.cwrap('GuidoGetNewPageDate','number', ['number','number']);
 
 GuidoGetPageMap_JSON = Module.cwrap('GuidoGetPageMap_JSON','number', ['number','number','number','number']);
 GuidoGetStaffMap_JSON = Module.cwrap('GuidoGetStaffMap_JSON','number', ['number','number','number','number','number']);
@@ -34,6 +34,29 @@ GuidoGetSystemMap_JSON = Module.cwrap('GuidoGetSystemMap_JSON','number', ['numbe
 GuidoDrawBoudingBoxes = Module.cwrap('GuidoDrawBoundingBoxes',null, ['number']);
 GuidoGetDrawBoundingBoxes = Module.cwrap('GuidoGetDrawBoundingBoxes', null, []);
 
+GuidoGetNewPageFormat = Module.cwrap('GuidoGetNewPageFormat', 'number', ['number', 'number']); 
+GuidoGetNewDefaultPageFormat = Module.cwrap('GuidoGetNewDefaultPageFormat', 'number', []); 
+GuidoSetDefaultPageFormat = Module.cwrap('GuidoSetDefaultPageFormat', null, ['number']);
+GuidoDeletePageFormat = Module.cwrap('GuidoDeletePageFormat', null, ['number']);
+GuidoResizePageToMusic = Module.cwrap('GuidoResizePageToMusic', 'number', ['number']);
+
+GuidoUnit2CM = Module.cwrap('GuidoUnit2CM', 'number', ['number']);
+GuidoCM2Unit = Module.cwrap('GuidoCM2Unit', 'number', ['number']);
+GuidoUnit2Inches = Module.cwrap('GuidoUnit2Inches', 'number', ['number']);
+GuidoInches2Unit = Module.cwrap('GuidoInches2Unit', 'number', ['number']);
+
 GuidoGetAR2GR = Module.cwrap('GuidoGetAR2GR', 'number', ['number', 'number']);
+GuidoUpdateGR = Module.cwrap('GuidoUpdateGR', 'number', ['number', 'number']); 
+
+GuidoGetVersionStr = Module.cwrap('GuidoGetVersionStr', 'number', []);
+GuidoCheckVersionNums = Module.cwrap('GuidoCheckVersionNums', 'number', ['number', 'number', 'number']);
+GuidoGetLineSpace = Module.cwrap('GuidoGetLineSpace', 'number', []);
+
+//GuidoGetNewDefaultLayoutSettings = Module.cwrap('GuidoGetNewDefaultLayoutSettings', 'number', ['number']);
+GuidoDeleteLayoutSettings = Module.cwrap('GuidoDeleteLayoutSettings', null, ['number']);
+
+GuidoGetArError = Module.cwrap('GuidoGetGrError', 'number', ['number']);
+GuidoGetGrError = Module.cwrap('GuidoGetGrError', 'number', ['number']);
+GuidoGetErrorString = Module.cwrap('GuidoGetErrorString', 'number', ['number']);
 
 GuidoShutdown = Module.cwrap('GuidoShutdown', null, []);
