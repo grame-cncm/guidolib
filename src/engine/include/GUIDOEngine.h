0298fd0c5114afbe15c650343ffdb0a26a738f24
guillaume <guillaume.gouilloux@gmail.com>
2015-02-04 12:27:21 +0100

    Add new API to pass layout settings and page format to create GR object. Layout settings are now passed to the createGR method instead of to be affected to static variables

--- src/engine/include/GUIDOEngine.h
+++ src/engine/include/GUIDOEngine.h
@@ -257,6 +257,17 @@ typedef struct
 } GuidoPageFormat;
 
 
+/**
+	The Graphic representation parameters.
+
+	This data structure contains a GuidoLayoutSettings and a GuidoPageFormat.
+  */
+typedef struct
+{
+	GuidoLayoutSettings layoutSettings;
+	GuidoPageFormat pageFormat;
+} GuidoGrParameters;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -344,13 +355,32 @@ representations.
     GUIDOAPI(GuidoErrCode)	GuidoAR2GR(ARHandler ar, const GuidoLayoutSettings* settings, GRHandler* gr);
 
 	/*!
-        Applies new layout settings to an existing Guido graphic representation.
+	 * \brief GuidoAR2GRParameterized. Transforms a Guido abstract representation into a Guido graphic representation.
+	 * It use given GuidoGrParameters with a layout settings and a page format. If a page format is used in gmn code,
+	 * it has priority over the GuidoGrParameters's page format.
+	 * \param ar a Abstract representation handler.
+	 * \param gp a GuidoGrParameters structure. If null default layout settings and page format are used.
+	 * \return a Graphic representation handler or null if an error occurs.
+	 */
+	GUIDOAPI(GRHandler) GuidoAR2GRParameterized(ARHandler ar, const GuidoGrParameters* gp);
+
+	/*!
+		Applies new layout settings to an existing Guido graphic representation.
 		\param gr the handler to the graphic representation.
 		\param settings a pointer to the settings for the graphic layout. If null, default
         settings are applied.
 		\return a Guido error code.
     */
     GUIDOAPI(GuidoErrCode)	GuidoUpdateGR( GRHandler gr, const GuidoLayoutSettings* settings);
+
+	/*!
+		Applies new layout settings and page format to an existing Guido graphic representation.
+		\param gr the handler to the graphic representation.
+		\param settings a pointer to the settings for the graphic representation. If null, default
+		settings are applied.
+		\return a Guido error code.
+	*/
+	GUIDOAPI(GuidoErrCode)	GuidoUpdateGRParameterized( GRHandler gr, const GuidoGrParameters* settings);
 
 	/*!
         Releases a Guido abstract representation.
