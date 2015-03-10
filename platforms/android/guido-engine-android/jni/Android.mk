aad3ba1db3905514c4a63530a17869ecdd7aae68
guillaume <guillaume.gouilloux@gmail.com>
2015-03-10 15:43:29 +0100

    Add android make to have a shared library for guidoengine without jni

---
+++ platforms/android/guido-engine-android/jni/Android.mk
@@ -0,0 +1,18 @@
+#### Shared library for Guido engine ####
+
+LOCAL_PATH := $(call my-dir)
+SRC := $(LOCAL_PATH)/../../../../src/engine
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := GUIDOEngine
+
+# SRC var is a trick to use wildcard in android.mk
+# if no wildcard, you have better to use relative path, conforming to android doc 
+LOCAL_SRC_FILES         := $(subst $(LOCAL_PATH)/,,$(wildcard $(SRC)/*/*.cpp))
+LOCAL_EXPORT_C_INCLUDES := $(addprefix $(SRC)/, include devices lib))
+LOCAL_C_INCLUDES        := $(subst $(LOCAL_PATH)/../,,$(wildcard $(SRC)/*/))
+LOCAL_CPPFLAGS          := -Dandroid -frtti -DINDEPENDENTSVG
+
+include $(BUILD_SHARED_LIBRARY)
+
