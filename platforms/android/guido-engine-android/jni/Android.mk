488bbcd6950c312edfa3e628e7fe66211e6f3f56
guillaume <guillaume.gouilloux@gmail.com>
2015-03-12 17:38:55 +0100

    Remove extra parathesis, but no change in the build

--- platforms/android/guido-engine-android/jni/Android.mk
+++ platforms/android/guido-engine-android/jni/Android.mk
@@ -10,7 +10,7 @@ LOCAL_MODULE := GUIDOEngine
 # SRC var is a trick to use wildcard in android.mk
 # if no wildcard, you have better to use relative path, conforming to android doc 
 LOCAL_SRC_FILES         := $(subst $(LOCAL_PATH)/,,$(wildcard $(SRC)/*/*.cpp))
-LOCAL_EXPORT_C_INCLUDES := $(addprefix $(SRC)/, include devices lib))
+LOCAL_EXPORT_C_INCLUDES := $(addprefix $(SRC)/, include devices lib)
 LOCAL_C_INCLUDES        := $(subst $(LOCAL_PATH)/../,,$(wildcard $(SRC)/*/))
 LOCAL_CPPFLAGS          := -Dandroid -frtti -DINDEPENDENTSVG
 
