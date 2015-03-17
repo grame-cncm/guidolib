0c82bc3bb8ad0cc78a46f8b2bd417920caba1714
guillaume <guillaume.gouilloux@gmail.com>
2015-03-17 09:15:26 +0100

    Add ndk version to ndk-build make files.

--- platforms/android/guido-engine-android/jni/Application.mk
+++ platforms/android/guido-engine-android/jni/Application.mk
@@ -1,4 +1,6 @@
 
 #APP_STL 			:= stlport_static
-APP_STL := gnustl_static
+APP_STL := gnustl_shared
+APP_PLATFORM := android-16
+
 #APP_ABI := x86
--- platforms/android/guido-engine-jni-android/jni/Application.mk
+++ platforms/android/guido-engine-jni-android/jni/Application.mk
@@ -1,6 +1,6 @@
-
 #APP_STL 			:= stlport_static
-APP_STL := gnustl_static
 #APP_ABI := x86
 
+APP_STL := gnustl_static
+APP_PLATFORM := android-16
 
