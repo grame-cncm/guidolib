1856e65db05fd45b0b96ebd82d762ad4a3f042bd
D. Fober <fober@grame.fr>
2017-10-10 12:55:31 +0200

    fix element bounding box

--- src/engine/graphic/GRIntens.cpp
+++ src/engine/graphic/GRIntens.cpp
@@ -16,9 +16,12 @@
 #pragma warning (disable : 4786)
 #endif
 
+#include <iostream>
 #include <string.h>
 
 #include "ARIntens.h"
+#include "TagParameterFloat.h"
+#include "MusicalSymbols.h"
 
 #include "GRIntens.h"
 #include "GRStaff.h"
@@ -30,92 +33,60 @@
 #include "VGDevice.h"
 #include "VGFont.h"
 
-GRIntens::GRIntens( GRStaff * inStaff, const ARIntens* abstractRepresentationOfIntens)
-  : GRTagARNotationElement(abstractRepresentationOfIntens, LSPACE)
+using namespace std;
+
+GRIntens::GRIntens( GRStaff * inStaff, const ARIntens* ar)
+  : GRTagARNotationElement(ar, LSPACE)
 {
-	assert(abstractRepresentationOfIntens);
+	assert(ar);
 
 	// this is not quite clear!
 	mNeedsSpring = 1;
 	sconst = SCONST_INTENS;
-
-	// spacing= 0.75 * LSPACE;
-	// obsolete
-	// spacing = 0;
-
 	mGrStaff = inStaff;
 
 	// Now try to figure out, which intensity was selected:
 	const char * cp = getARIntens()->getText().c_str();
+	if (!strcmp(cp,"p"))		mSymbol = INTENS_P;
+	else if (!strcmp(cp,"f") )	mSymbol = INTENS_F;
+	else if (!strcmp(cp,"ff"))	mSymbol = INTENS_FF;
+	else if (!strcmp(cp,"fff"))	mSymbol = INTENS_FFF;
+	else if (!strcmp(cp,"ffff"))mSymbol = INTENS_FFFF;
+	else if (!strcmp(cp,"mf"))	mSymbol = INTENS_MF;
+	else if (!strcmp(cp,"mp"))	mSymbol = INTENS_MP;
+	else if (!strcmp(cp,"sf"))	mSymbol = INTENS_SF;
+	else if (!strcmp(cp,"pp"))	mSymbol = INTENS_PP;
+	else if (!strcmp(cp,"ppp"))	mSymbol = INTENS_PPP;
+	else if (!strcmp(cp,"pppp"))mSymbol = INTENS_PPPP;
 
-//	rcount = 0;
-
-	if (!strcmp(cp,"p"))
-		mSymbol = INTENS_P;
-
-	else if (!strcmp(cp,"f") )
-		mSymbol = INTENS_F;
-
-	else if (!strcmp(cp,"ff"))
-		mSymbol = INTENS_FF;
-
-	else if (!strcmp(cp,"fff"))
-		mSymbol = INTENS_FFF;
-
-	else if (!strcmp(cp,"ffff"))
-		mSymbol = INTENS_FFFF;
-
-	else if (!strcmp(cp,"mf"))
-		mSymbol = INTENS_MF;
-
-	else if (!strcmp(cp,"mp"))
-		mSymbol = INTENS_MP;
-
-	else if (!strcmp(cp,"sf"))
-		mSymbol = INTENS_SF;
-
-	else if (!strcmp(cp,"pp"))
-		mSymbol = INTENS_PP;
-
-	else if (!strcmp(cp,"ppp"))
-		mSymbol = INTENS_PPP;
-
-	else if (!strcmp(cp,"pppp"))
-		mSymbol = INTENS_PPPP;
-
-	if (mSymbol != 0)
-	{
+	if (mSymbol != 0) {
 		float x = 0;
 		float y = 0;
+		float dx = 0;
+		float dy = 0;
+
 		if( gGlobalSettings.gDevice )
 			FontManager::gFontScriab->GetExtent( mSymbol, &x, &y, gGlobalSettings.gDevice );
 
-		mBoundingBox.left = (GCoord)(LSPACE / 4);
-		mBoundingBox.right = (GCoord)(x);
-		mBoundingBox.top = (GCoord)(y);
+		const TagParameterFloat* p = ar->getDX();
+		dx = p->getValue();
+		p = ar->getDY();
+		if (p) dy = -p->getValue();
 
-//		if (rcount!=0) mBoundingBox.right *= rcount;
-
-		mBoundingBox.bottom = (GCoord)(4*LSPACE);
+		mBoundingBox.left  = dx;
+		mBoundingBox.right = x + dx;
+		mBoundingBox.top = LSPACE * -1.5;
+		mBoundingBox.bottom = LSPACE/2;
 	}
-	else
-	{
-		mBoundingBox.Set( 0, 0, 0, 0 );
-	}
+	else mBoundingBox.Set( 0, 0, 0, 0 );
 
 	mLeftSpace = 0;
 	mRightSpace = 0;
 
 	// no referencePosition?
 	// probably dependant on Type!
-
 	const float curLSPACE = mGrStaff ? mGrStaff->getStaffLSPACE() : LSPACE;
-
 	mPosition.y = (GCoord)(6 * curLSPACE);
-}
-
-GRIntens::~GRIntens()
-{
 }
 
 // -----------------------------------------------------------------------------
@@ -136,7 +107,7 @@ void GRIntens::OnDraw(VGDevice & hdc) const
 
 const ARIntens* GRIntens::getARIntens() const
 {
-	return /*dynamic*/static_cast<const ARIntens*>(getAbstractRepresentation());
+	return static_cast<const ARIntens*>(getAbstractRepresentation());
 }
 
 
