11f96e3dc827a3431fa136c6b7b4e915aec7ab61
Dominique Fober <fober@grame.fr>
2019-02-26 09:50:41 +0100

    add compiler runtime option

--- build/deploy.bat
+++ build/deploy.bat
@@ -2,8 +2,8 @@
 echo "Deploying applications"
 
 set cmd="dir /b/s GuidoEditor.exe"
-FOR /F "tokens=*" %%i IN (' %cmd% ') DO  windeployqt %%i
+FOR /F "tokens=*" %%i IN (' %cmd% ') DO  windeployqt %%i --compiler-runtime
 
 set cmd="dir /b/s GuidoSceneComposer.exe"
-FOR /F "tokens=*" %%i IN (' %cmd% ') DO  windeployqt %%i
+FOR /F "tokens=*" %%i IN (' %cmd% ') DO  windeployqt %%i --compiler-runtime
 
