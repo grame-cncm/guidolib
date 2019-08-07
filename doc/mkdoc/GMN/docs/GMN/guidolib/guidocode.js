73b51588f852785ea6a04684c3a3c669c846dc2d
Dominique Fober <fober@grame.fr>
2019-08-07 16:07:36 +0200

    support for multiple pages

--- doc/mkdoc/GMN/docs/GMN/guidolib/guidocode.js
+++ doc/mkdoc/GMN/docs/GMN/guidolib/guidocode.js
@@ -4,14 +4,23 @@ var gSettings;
 var gParser;
 
 function processGMNDiv( div, parser) {
-	var content = div.getElementsByTagName('pre')[0].innerHTML;
+	var pre = div.getElementsByTagName('pre')[0];
+	var content = pre.innerHTML;
  	content = content.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&");
 //console.log ("processGMNDiv " + content);
 	var ar = guidoEngine.string2AR(parser, content);
 	if (ar) {
 		var gr = guidoEngine.ar2gr(ar);
 // 		div.innerHTML = guidoEngine.gr2SVG(gr, 1, false, 0);
-		div.innerHTML = guidoEngine.gr2SVGColored (gr, 1, 0, 0, 0, false);
+		var p = guidoEngine.getPageCount (gr);
+		if (p==1) div.innerHTML = guidoEngine.gr2SVGColored (gr, 1, 0, 0, 0, false);
+		else {
+			div.removeChild(pre);
+			for (i=1; i <=p; i++) {
+			var pagediv = document.getElementById('p'+i)
+			pagediv.innerHTML = guidoEngine.gr2SVGColored (gr, i, 0, 0, 0, false);
+			}
+		}
 		guidoEngine.freeGR(gr);
 		guidoEngine.freeAR(ar);		
 	}
