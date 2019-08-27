a7366c3b8b353ee5baa8656e32090fa8634b0348
D. Fober <fober@grame.fr>
2019-08-27 10:01:39 +0200

    js tests

---
+++ lang/javascript/npm/testAPI.node.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+const fs = require ("fs");
+const GuidoEngine = require ("./guidoengine");
+const run = require ("./testAPI");
+
+var engine = new GuidoEngine().initialize ("./libGUIDOEngine").then (doit);
+
+function log(str) 		{ console.log (str); }
+function svg(str, name) { 
+	fs.writeFileSync (name + ".svg", str, "utf8" );
+}
+
+function doit (engine) {
+	console.log( "Guido Engine version " + engine.getVersion().str);
+ 	run ( engine, log, svg );
+}
