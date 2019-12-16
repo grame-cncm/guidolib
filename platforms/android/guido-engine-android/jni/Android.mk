dc9ef5108285a359252c8ab8721c7f1dbe7d50ec
D. Fober <fober@grame.fr>
2019-12-16 23:04:06 +0100

    android update

--- platforms/android/guido-engine-android/jni/Android.mk
+++ platforms/android/guido-engine-android/jni/Android.mk
@@ -12,8 +12,9 @@ LOCAL_MODULE := GUIDOEngine
 LOCAL_SRC_FILES         := $(subst $(LOCAL_PATH)/,,$(wildcard $(SRC)/*/*.cpp))
 LOCAL_EXPORT_C_INCLUDES := $(addprefix $(SRC)/, include devices lib)
 LOCAL_C_INCLUDES        := $(subst $(LOCAL_PATH)/../,,$(wildcard $(SRC)/*/))
-LOCAL_CPPFLAGS          := -Dandroid -frtti -DINDEPENDENTSVG
-LOCAL_LDLIBS += -latomic
+LOCAL_CPPFLAGS          := -Dandroid -frtti -DINDEPENDENTSVG -fexceptions
+LOCAL_CPP_FEATURES 	+= exceptions
+LOCAL_LDLIBS 		+= -latomic
 
 include $(BUILD_SHARED_LIBRARY)
 
--- platforms/android/guido-engine-android/jni/Application.mk
+++ platforms/android/guido-engine-android/jni/Application.mk
@@ -2,6 +2,7 @@
 APP_STL := c++_shared
 APP_PLATFORM := android-26
 APP_CPPFLAGS += -std=c++11
-APP_STL := c++_static
+APP_CPPFLAGS += -fexceptions
+#APP_STL := c++_static
 
 #APP_ABI := armeabi # armeabi-v7a arm64-v8a x86 x86_64
