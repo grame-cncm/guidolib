f7b988d9be261ad4b1f6c723a87308a2b2d5f53c
Dominique Fober <fober@grame.fr>
2020-01-29 16:20:54 +0100

    build all ABIs APP_PLATFORM is an optional parameter

--- platforms/android/guido-engine-android/jni/Application.mk
+++ platforms/android/guido-engine-android/jni/Application.mk
@@ -1,8 +1,8 @@
 
-APP_STL := c++_shared
-APP_PLATFORM := android-26
+APP_PLATFORM ?= android-22
 APP_CPPFLAGS += -std=c++11
 APP_CPPFLAGS += -fexceptions
-#APP_STL := c++_static
+#APP_STL := c++_shared
+APP_STL := c++_static
 
-#APP_ABI := armeabi # armeabi-v7a arm64-v8a x86 x86_64
+APP_ABI := all
