52caa0d4b1ca5e6adb147cd12e372c20a4e8193f
D. Fober <fober@grame.fr>
2022-03-31 18:56:54 +0200

    remove old commented code

--- src/engine/abstract/TagParameterFloat.cpp
+++ src/engine/abstract/TagParameterFloat.cpp
@@ -30,7 +30,6 @@ using namespace std;
 TagParameterFloat::TagParameterFloat(const TagParameterFloat & tpf)
 	: TagParameter(tpf)
 {
-//	if (kUnitLen) strncpy (fUnit, tpf.fUnit, kUnitLen);
 	fUnittag = tpf.fUnittag;
 	fValue = tpf.fValue;
 	fUnit = tpf.getUnit();
@@ -42,7 +41,6 @@ TagParameterFloat::TagParameterFloat(float val)		 : fUnittag(false), fValue(val)
 void  TagParameterFloat::set( const TagParameterFloat & in )
 {
 	TagParameter::set( in );
-//	if (kUnitLen) strncpy(fUnit, in.fUnit, kUnitLen);
 	fUnit = in.getUnit();
 	fUnittag = in.fUnittag;
 	fValue = in.fValue;
@@ -68,15 +66,6 @@ void TagParameterFloat::reset(float inFloatValue, const char * inUnit)
 		fUnit.clear();
 		fUnittag = false;
 	}
-	
-//	fUnittag = false;
-//	fValue = inFloatValue;
-//	fUnit[0] = '\0';
-//	if(!strncmp(fUnit, inUnit, kUnitLen)) {
-//		if (kUnitLen) strncpy(fUnit, inUnit, kUnitLen);
-//		fUnit[2] = 0;
-//		fUnittag = true;
-//	}
 }
 
 const TYPE_FLOATPARAMETER TagParameterFloat::getValue(float curLSPACE) const
@@ -103,32 +92,6 @@ const TYPE_FLOATPARAMETER TagParameterFloat::getValue(float curLSPACE) const
             // and not directly the value (like convertValue).
         }
 	}
-
-//	if (fUnittag)
-//	{
-//		char tmpunit[kUnitLen];
-//		if (kUnitLen) strncpy(tmpunit, fUnit, kUnitLen);
-//		if (tmpunit[0] == 0)
-//		{
-//			// no unit is given -> then
-//			// we take the unit specified by the units-tag ...
-//			if (kUnitLen) strncpy(tmpunit, ARUnits::getUnit().c_str(), kUnitLen);
-//		}
-//		if (!strcmp(tmpunit,"hs"))
-//			return ((float) (fValue * curLSPACE * 0.5f));
-//		
-//        double result;
-//		bool conversionOk = gd_convertUnits((double) fValue, tmpunit, "cm", result);
-//		
-//        if (conversionOk)
-//            return ((float)result * kCmToVirtual);
-//        else
-//        {
-//            // Failure. Thing is that getValue should returns an errorCode, 
-//            // and not directly the value (like convertValue).
-//        }
-//	}
-
 	return fValue;
 }
 
@@ -147,55 +110,11 @@ void TagParameterFloat::setValue(const char * p)
 		fUnittag = true;
 	}
 	else fValue = (float)atof(p);
-
-	// this is a difficult function.
-	// It must set the parameter and do it a unit-transform, if necessary ...
-	// first extract the unit.
-//	const size_t length = strlen(p);
-//	if (length == 0)  {  
-//		fValue = 0;
-//		return;
-//	}
-//
-//	size_t i = length - 1;
-//	while ( !isdigit((int) p[i]) ) --i;
-//
-//	// - Allocate a temp string table. It tryes to do it on the stack
-//	const size_t kStackStrLen = 64;
-//	char tmpStack [ kStackStrLen ];		// stack temp string
-//	char * tmpHeap = 0;					// heap temp string
-//
-//	char * tmp = 0;						// work string
-//	if(( i + 2 ) >= kStackStrLen )
-//	{
-//		tmpHeap = new char [ i + 2 ];	// Too long to convert on the stack, we use allocate a string on the heap
-//		tmp = tmpHeap;
-//	}									
-//	else tmp = tmpStack;	
-//			
-//	// - Copy required chars
-//	strncpy( tmp, p, i+2 );
-//	// - Convert them to float
-//	fValue = (float) atof(tmp);
-// 	delete [] tmpHeap;
-//
-//	// we need to to a conversion of units ...
-//	if (i != length - 1)
-//	{
-//		if (kUnitLen) strncpy(fUnit, &p[i+1], kUnitLen);
-//		fUnit[2] = 0;
-//		fUnittag = true;
-//	}
 }
 
 void TagParameterFloat::setUnit(const char * unit)
 {
-	// one should probable check the unit
-	// here -> if no valid unit is given
-	// use cm/inches or whatever ...
-//	if (kUnitLen) strncpy(fUnit,un,kUnitLen);
 	fUnit = unit;
-//	fUnit[2] = 0;
 	fUnittag = true;
 }
 
@@ -211,19 +130,6 @@ bool TagParameterFloat::copyValue(const TagParameter * tp)
 	else fUnit.clear();
 	fValue = tpf->fValue;		
 	return true;
-
-//	if (tpf) {
-//		if (!fUnittag && tpf->fUnit[0] != 0)
-//			return false;
-//	
-//		if (fUnittag && kUnitLen)
-//			strncpy(fUnit, tpf->fUnit,kUnitLen);
-//		else
-//			fUnit[0] = '\0';
-//		fValue = tpf->fValue;		
-//	}
-//	else return false;
-//	return true;
 }
 
 bool TagParameterFloat::convertValue(float fromValue, double &toValue, const char * unit, float curLSPACE)
